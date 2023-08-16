5fb45a43e11989189edadd388ceb116c7e3c736a
mansur <20260249+mansurGI@users.noreply.github.com>
2023-08-16 19:14:13 +0200

    upload.sh g++ new flag -fPIC (Position Independed Code)

--- upload.sh
+++ upload.sh
@@ -5,6 +5,6 @@ scp main.cpp fvds:/usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
 ssh fvds "mysql -e 'DROP FUNCTION IF EXISTS faker;'
 service mysqld restart
 rm -f /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so
-g++ -shared -I/usr/include/mysql -o /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so /usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
+g++ -fPIC -shared -I/usr/include/mysql -o /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so /usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
 mysql -e 'CREATE FUNCTION faker RETURNS string SONAME '\''faker.so'\'';'"
 
