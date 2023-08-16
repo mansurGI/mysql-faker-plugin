49b858294ee895d822bd73874814d2356efc02fa
mansur <20260249+mansurGI@users.noreply.github.com>
2023-08-16 15:17:43 +0200

    basic implementation of random fake name, added mysql restart in upload script

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
-#include "mysql.h"
-#include "cstring"
-
+#include <mysql.h>
+#include <string>
+#include <ctime>
+#include <vector>
+	
 extern "C" bool faker_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
   return false;
 }
 
+extern "C" void faker_deinit(UDF_INIT *initid);
+
 extern "C" char *faker(UDF_INIT *initid, UDF_ARGS *args, char *result, unsigned long *length, char *is_null, char *error) {
-  memcpy(result, "result string", 13);
-  *length = 13;
+  std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+  std::vector<std::string> names = {"Jhon", "Steve", "Tatiana", "Kelvin", "Marry", "Gwen"};
+  
+  std::string name = names[std::rand() % names.size()];
+		
+  name.copy(result, name.size());
+  *length = name.size();
 
   return result;
 }
-
-extern "C" void faker_deinit(UDF_INIT *initid);
 
--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 
 scp main.cpp fvds:/usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
 
-ssh fvds "rm -f /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so
-  g++ -shared -I/usr/include/mysql -o /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so /usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
-  mysql -e 'DROP FUNCTION IF EXISTS faker; CREATE FUNCTION faker RETURNS integer SONAME '\''faker.so'\'';'"
+ssh fvds "mysql -e 'DROP FUNCTION IF EXISTS faker;'
+service mysqld restart
+rm -f /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so
+g++ -shared -I/usr/include/mysql -o /usr/lib/x86_64-linux-gnu/mariadb19/plugin/faker.so /usr/lib/x86_64-linux-gnu/mariadb19/plugin/main.cpp
+mysql -e 'CREATE FUNCTION faker RETURNS string SONAME '\''faker.so'\'';'"
 
